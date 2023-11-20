16a18d13a67afa46e2c94a8db64520160c99674e
GuzH09 <guzh09@gmail.com>
2023-11-20 13:45:25 -0300

    First website commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: 
+    {
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle, rgba(76,61,61,1) 0%, rgba(45,42,42,1) 100%)',
+        'radial-gradient-transparent': 'none',
+      },
+      fontFamily: {
+        'bebasneue': ['Bebas Neue', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
